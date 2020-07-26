400294f06d3c0d7e3a980a050b0b21e4780dca65
Qais Yousef <qyousef@layalina.io>
2020-07-26 01:39:14 +0100

    Add BPF Compiler Collection (BCC) Package
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

---
+++ package/bcc/bcc.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# BPF Compiler Collection (BCC)
+#
+################################################################################
+
+BCC_VERSION = v0.15.0
+BCC_SITE = https://github.com/iovisor/bcc
+BCC_SITE_METHOD = git
+BCC_GIT_SUBMODULES = YES
+BCC_LICENSE = Apache-2.0
+BCC_LICENSE_FILES = LICENSE.txt
+BCC_INSTALL_STAGING = YES
+BCC_DEPENDENCIES = host-bison host-flex elfutils clang llvm
+
+BCC_CONF_OPTS += \
+	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -isystem $(HOST_DIR)/include"
+
+$(eval $(cmake-package))
