f27e8932795f2884e41f589dec98ae1534a48102
Qais Yousef <qyousef@layalina.io>
2020-08-07 23:41:31 +0100

    bcc: add dependency on tar
    to untar kheaders
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- package/bcc/bcc.mk
+++ package/bcc/bcc.mk
@@ -11,7 +11,7 @@ BCC_GIT_SUBMODULES = YES
 BCC_LICENSE = Apache-2.0
 BCC_LICENSE_FILES = LICENSE.txt
 BCC_INSTALL_STAGING = YES
-BCC_DEPENDENCIES = host-bison host-flex host-zlib zlib elfutils clang llvm
+BCC_DEPENDENCIES = host-bison host-flex host-zlib zlib tar elfutils clang llvm
 
 ifeq ($(BR2_PACKAGE_PYTHON),y)
 BCC_DEPENDENCIES += python
