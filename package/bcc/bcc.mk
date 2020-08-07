df54b45cb3fddf0577370faa52dd2d6f0cbbd5d8
Qais Yousef <qyousef@layalina.io>
2020-08-07 22:49:37 +0100

    bcc: aarch64 requires glibc
    And allow building against python3
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- package/bcc/bcc.mk
+++ package/bcc/bcc.mk
@@ -13,6 +13,12 @@ BCC_LICENSE_FILES = LICENSE.txt
 BCC_INSTALL_STAGING = YES
 BCC_DEPENDENCIES = host-bison host-flex host-zlib zlib elfutils clang llvm
 
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+BCC_DEPENDENCIES += python
+else
+BCC_DEPENDENCIES += python3
+endif
+
 BCC_CONF_OPTS += \
 	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -isystem $(HOST_DIR)/include"
 
