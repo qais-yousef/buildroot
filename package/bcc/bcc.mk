d743f55a5faaed3bfe5ee66d67f3d97358a018a0
Qais Yousef <qais.yousef@arm.com>
2020-09-18 15:59:34 +0100

    Add BPF Compiler Collection (BCC) Package
    BCC is a toolkit for creating efficient kernel tracing and manipulation
    programs, and includes several useful tools and examples.
    
    Only tested to work on x86_64 and aarch64.
    
    I had issues getting i386 and aarch32 to work. More archs support can be
    added later hopefully after this initial support.
    
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>

---
+++ package/bcc/bcc.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# BPF Compiler Collection (BCC)
+#
+################################################################################
+
+BCC_VERSION = v0.15.0
+BCC_SITE = https://github.com/iovisor/bcc
+BCC_SITE_METHOD = git
+BCC_GIT_SUBMODULES = YES
+BCC_LICENSE = Apache-2.0
+BCC_LICENSE_FILES = LICENSE.txt
+BCC_INSTALL_STAGING = YES
+BCC_DEPENDENCIES = host-bison host-flex host-zlib zlib tar elfutils clang llvm
+
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+BCC_DEPENDENCIES += python
+else
+BCC_DEPENDENCIES += python3
+endif
+
+BCC_CONF_OPTS += \
+	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -isystem $(HOST_DIR)/include"
+
+$(eval $(cmake-package))
